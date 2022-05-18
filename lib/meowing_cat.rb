148292fd22be31b8ed32fcb7369ba805bbd9b180
Rebecca Sissman <rsissman15@aol.com>
2022-05-18 12:34:11 -0400

    commit tree 70097a1087a56b8f3be609db1c37b44932688e31

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,22 @@
 ## code your solution here. 
+require 'pry'
+
+class Cat
+    attr_reader :meow
+    attr_accessor :name
+
+    def name=(name)
+        @name=name
+    end
+    def meow
+        puts "meow!"
+    end
+end
+
+
+
+
+c1=Cat.new
+c1.name="Tucker"
+c1.meow
+
